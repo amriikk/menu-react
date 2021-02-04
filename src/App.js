51fd536fd338d71314c973f71dc990b9af4f9243
John Trujillo <amriikk@gmail.com>
2021-02-03 23:54:57 -0800

    update iflterItems function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
@@ -6,8 +6,22 @@ import items from './data';
 const AllCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(AllCategories);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
-    <h2>Menu Project Setup</h2>
+    <main>
+      
+    </main>
   );
 };
 
